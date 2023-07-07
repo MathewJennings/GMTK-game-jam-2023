f366ae4acd9a2832d8e55bd13e3a3696fe2d848c
Mathew Jennings <mathew.percussion@gmail.com>
2023-07-07 19:49:29 -0400

    Add player animations and house

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,18 +9,46 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed;
 
     private Rigidbody2D rigidbody;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float h = Input.GetAxisRaw("Horizontal");
+        Debug.Log(h);
         float v = Input.GetAxisRaw("Vertical");
+        if (h > 0)
+        {
+            animator.SetBool("walkRight", true);
+        }
+        else if (h < 0)
+        {
+            animator.SetBool("walkLeft", true);
+        }
+        else
+        {
+            animator.SetBool("walkRight", false);
+            animator.SetBool("walkLeft", false);
+        }
+        if (v > 0)
+        {
+            animator.SetBool("walkUp", true);
+        } else if (v < 0)
+        {
+            animator.SetBool("walkDown", true);
+        }
+        else
+        {
+            animator.SetBool("walkUp", false);
+            animator.SetBool("walkDown", false);
+        }
         rigidbody.velocity = new Vector2(h * speed, v * speed);
     }
 }
