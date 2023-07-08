245c1a43b431b78bf970f6dca1c11b656e04e617
Felix Lee <flee135@users.noreply.github.com>
2023-07-07 18:21:21 -0700

    Add prices to items

--- Assets/Scripts/Inventory/AllItems.cs
+++ Assets/Scripts/Inventory/AllItems.cs
@@ -6,11 +6,11 @@ public class AllItems : MonoBehaviour
 {
     private List<Item> allItems = new List<Item>
     {
-        new Item("gold", Item.ItemType.Gold, 0),
-        new Item("fooSeed", Item.ItemType.Seed, 0, "fooCrop"),
-        new Item("fooCrop", Item.ItemType.Crop, 0, "fooSeed"),
-        new Item("barSeed", Item.ItemType.Seed, 0, "barCrop"),
-        new Item("barCrop", Item.ItemType.Crop, 0, "barSeed")
+        new Item("gold", Item.ItemType.Gold, 0, 1),
+        new Item("fooSeed", Item.ItemType.Seed, 0, 10, "fooCrop"),
+        new Item("fooCrop", Item.ItemType.Crop, 0, 10, "fooSeed"),
+        new Item("barSeed", Item.ItemType.Seed, 0, 10, "barCrop"),
+        new Item("barCrop", Item.ItemType.Crop, 0, 10, "barSeed"),
     };
 
     public List<Item> GetAllItems()
