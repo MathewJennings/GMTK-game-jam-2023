18b1a09a459911e9dfd14b6ca2114fb69fbe590d
Jennifer Liao <38355986+jliao13@users.noreply.github.com>
2023-07-09 10:47:10 -0400

    add wealthy merchant sprites

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -368,7 +368,7 @@ public void Start()
                 // Clear out all listeners on buttons to make sure we're not accumulating multiple
                 // listeners on a single button.
                 ResetChoiceButtons();
-                dayTimeController.SetPausedTime();
+                dayTimeController.SetPausedTime(true);
                 VisualElement root = uidoc.rootVisualElement;
                 root.Q<Label>("label").text = nextEvent.template.dialogText;
                 VisualElement eventButtonsRoot = eventButtonsDoc.rootVisualElement;
